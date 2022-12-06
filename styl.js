0118cd001a1cf577940049c09f9c03ca92fb6f70
Ecenur Kaya <kaya.ecenur02@gmail.com>
2022-12-07 01:56:39 +0300

    js codes

---
+++ styl.js
@@ -0,0 +1,19 @@
+document.querySelector("#giris").onclick = function()
+{
+    window.location.href="home.html";
+}
+
+document.querySelector("#home").onclick = function()
+{
+    window.location.href="hesabim.html";
+}
+
+document.querySelector("#signup").onclick = function()
+{
+    window.location.href="index.html";
+}
+/*const pointDOM = document.querySelector('#point')
+document.querySelector("#show").onclick = function()
+{
+    document.getElementById("#point").value="a";
+}*/
